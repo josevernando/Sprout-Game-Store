f352a536a2b6dc98721ca95fa31f8b9145d130ee
fariduri <63889035+fariduri@users.noreply.github.com>
2022-04-06 09:54:52 +0700

    Tambah Classs Account & elemen dasar

---
+++ Sprout Website/classes/Account.js
@@ -0,0 +1,14 @@
+class Account 
+{
+    constructor(username, password, account_id, email, fullName, isDeveloper)
+    {
+        this.username = username;
+        this.password = password;
+        this.account_id = account_id;
+        this.email = email;
+        this.fullName = fullName;
+        this.isDeveloper = isDeveloper;
+    }
+
+    
+}
