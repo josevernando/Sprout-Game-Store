c8541b8fe1ada91d7667489552d0fb7d38b81203
fariduri <fadriduck@gmail.com>
2022-05-18 07:39:32 +0700

    Set up home page

--- sproutg/base/urls.py
+++ sproutg/base/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
 ]
--- sproutg/base/views.py
+++ sproutg/base/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(req):
+    return render(request=req, template_name='base/home.html')
