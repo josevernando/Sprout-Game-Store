e034e8e9e2553e0fd245791d70c2363734bc8ee4
fariduri <fadriduck@gmail.com>
2022-05-18 09:43:47 +0700

    Add login function

--- sproutg/base/urls.py
+++ sproutg/base/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
 ]
--- sproutg/base/views.py
+++ sproutg/base/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect ('home')
+        
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(req):
     return render(request=req, template_name='base/home.html')
