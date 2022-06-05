f779160edfc3f8dd6b2f88b214408350a04fddf4
fariduri <fadriduck@gmail.com>
2022-06-05 19:24:08 +0700

    Renew Database, add custom User model

--- sproutg/base/admin.py
+++ sproutg/base/admin.py
@@ -1,14 +1,11 @@
-from atexit import register
 from django.contrib import admin
-from .models import Developer, Game, Customer, Transaksi, Review
+from .models import Developer, Game, Customer, Transaksi, Review, User
 
+admin.site.register(User)
 admin.site.register(Developer)
 admin.site.register(Game)
 admin.site.register(Transaksi)
 admin.site.register(Review)
 admin.site.register(Customer)
 
-# fariduri dongkrakKodok29
-# simbo cikarang229
-# putrigaming diskon75
-# Register your models here.
+# fariduri makassar25
--- sproutg/base/models.py
+++ sproutg/base/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
 class Developer(models.Model):
     name = models.CharField(max_length=200)
     # GameList =
-    
+  
     def __str__(self):
         return self.name
-
+    
+    
 class Game(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -16,42 +22,45 @@ class Game(models.Model):
     price = models.FloatField()
     verified = models.BooleanField()
     # reviews = 
-    
+  
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+  
     def __str__(self):
         return self.name
-    
+  
+  
 class Review(models.Model):
     name = models.CharField(max_length=200)
     text = models.TextField()
-    
+  
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+  
     account = models.ForeignKey(User, on_delete=models.CASCADE)
     gameid = models.ForeignKey(Game, on_delete=models.CASCADE)
-    
+  
     def __str__(self):
         return self.name
-
+    
+    
 class Transaksi(models.Model):
     name = models.CharField(max_length=200)
     customerid = models.ForeignKey(User, on_delete=models.CASCADE)
     # gameid =
-    
+  
     created = models.DateTimeField(auto_now_add=True)
-    
+  
     def __str__(self):
         return self.name
-
+    
+    
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     cart = models.ManyToManyField(Game, related_name='cart', blank=True)
     myGames = models.ManyToManyField(Game, related_name='myGames', blank=True)
     wishList = models.ManyToManyField(Game, related_name='wishList', blank=True)
     # transaksi =
-    
+  
     def __str__(self):
         return self.user.username + "'s customer model"
